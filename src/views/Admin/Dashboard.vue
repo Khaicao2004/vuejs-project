600b01f3044265fa7962c089ae2affed747597cb
khaicq <khaicq@newit.co.jp>
2024-12-11 17:15:27 +0700

    v-bind and v-model

--- src/views/Admin/Dashboard.vue
+++ src/views/Admin/Dashboard.vue
@@ -1,8 +1,35 @@
-<script>
-
-</script>
 <template>
    <h1>Đây là dashboard!</h1>
+   <p>Using text interpolation: {{ rawHtml }}</p>
+   <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+   <div :id>222</div>
+   <button :disabled="isButtonDisabled" @click="handleClick">Button</button>
+   <div v-bind="objectOfAttrs">BC</div>
 </template>
 
+<script>
+   export default{
+      data() {
+         return {
+            rawHtml: '<span><strong>hello</strong></span>',
+            id: 'kahahah',
+            isButtonDisabled: false,
+            objectOfAttrs: {
+            id: 'container',
+            class: 'wrapper'
+    }
+         }
+      },
+      methods: {
+         handleClick(){
+            this.isButtonDisabled = true;
+           console.log(this.isButtonDisabled);
+           
+         }
+      },
+      mounted() {
+         console.log(this.isButtonDisabled);
+      },
+   }
+</script>
 <style lang="css" scoped></style>
