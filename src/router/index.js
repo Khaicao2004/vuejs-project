45262fb70c93c2ed2ae7b5cd19e420bb1be6e416
khaicq <khaicq@newit.co.jp>
2024-12-26 17:05:50 +0700

    day 2 with vuejs and change folder router

--- src/router/index.js
+++ src/router/index.js
@@ -1,97 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-  
+import { createRouter, createWebHistory } from "vue-router";
+import adminRoutes from "./adminRoutes";
+import learnRoutes from "./learnRoutes";
+import clientRoutes from "./clientRoutes";
+
+const routes = [...adminRoutes, ...clientRoutes, ...learnRoutes];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('../views/admin/HomePage.vue'),
-      children: [
-        {
-          path: '',
-          name: 'dashboard',
-          component: () => import('../views/admin/Dashboard.vue'),
-        },
-        {
-          path: 'categories',
-          name: 'categories',
-          component: () => import('../views/admin/categories/Category.vue'),
-        },
-        {
-          path: 'accounts',
-          name: 'accounts',
-          component: () => import('../views/admin/users/Account.vue'),
-        },
-        {
-          path: 'accounts/:id',
-          name: 'accounts-detail',
-          component: () => import('../views/admin/users/AccountDetail.vue'),
-        }, 
-        {
-          path: 'test',
-          name: 'test',
-          component: () => import('../views/admin/Test.vue'),
-        },
-        {
-          path: 'accounts/create',
-          name: 'accounts-create',
-          component: () => import('../views/admin/users/Create.vue'),
-        },
-        {
-          path: 'categories/create',
-          name: 'categories-create',
-          component: () => import('../views/admin/categories/Create.vue'),
-        },
-      ]
-    },
-    {
-      path: '/client',
-      name: 'client',
-      component: () => import('../views/client/Home.vue'),
-    },
-    {
-      path: '/learn',
-      name: 'learn',
-      component: () => import('../views/learn/Learn.vue'),
-      children: [
-        {
-          path: 'unit1',
-          name: 'unit1',
-          component: () => import('../views/learn/day1/Unit1.vue'),
-        },
-        {
-          path: 'unit2',
-          name: 'unit2',
-          component: () => import('../views/learn/day1/Unit2.vue'),
-        },
-        {
-          path: 'unit3',
-          name: 'unit3',
-          component: () => import('../views/learn/day1/Unit3.vue'),
-        },
-        {
-          path: 'unit4',
-          name: 'unit4',
-          component: () => import('../views/learn/day1/Unit4.vue'),
-        },
-        {
-          path: 'unit5',
-          name: 'unit5',
-          component: () => import('../views/learn/day1/Unit5.vue'),
-        },
-        {
-          path: 'unit6',
-          name: 'unit6',
-          component: () => import('../views/learn/day1/Unit6.vue'),
-        }, {
-          path: 'unit7',
-          name: 'unit7',
-          component: () => import('../views/learn/day1/Unit7.vue'),
-        },
-      ]
-    }
-  ],
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/learn/day2/Unit8.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1 class="text-center mt-5 mb-5">Real-Time clock</h1>
+    <div class="d-flex justify-content-center time">
+        <!-- <span class="text-center">{{  currentTime }}</span> -->
+    </div>
+</template>
+
+<!-- <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+    const currentTime = ref('');
+    let intervalId = null;
+    const updateClock = () => {
+        const now = new Date();
+        const date = String(now.getDate()).padStart(2, "0");
+        const dayOfWeek = now.getDay();
+        const days = ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"];
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const year = now.getFullYear();
+        const hours = String(now.getHours()).padStart(2,"0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+        currentTime.value = `${days[dayOfWeek]} - ${date}/${month}/${year} - ${hours}:${minutes}:${seconds}`;        
+    };
+    onMounted(() => {
+        updateClock();
+        intervalId =  setInterval(updateClock, 1000);        
+    });
+
+    onUnmounted(() => {
+        clearInterval(intervalId); // dừng interval khi component bị hủy
+    });
+</script> -->
+
+<style lang="css" scoped>
+    .time{  
+        font-size: 50px;
+        font-weight: bold;
+    }
+</style>
